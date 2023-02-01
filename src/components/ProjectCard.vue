33bbe82c360dc69b9dddf344a0a9c49400346f67
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-02-01 22:24:07 +0100

    Aggiunge stampa img card Utilizza placeholder (implementato a backend) se img non presente

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -14,9 +14,9 @@ export default {
 </script>
 
 <template>
-    <div class="card">
+    <div class="card h-100">
         <div class="text-center">
-            <img src="" class="card-img-top" alt="">
+            <img v-if="info.image_url" :src="info.image_url" class="card-img-top" :alt="info.title">
         </div>
         <div class="card-body">
             <h4 class="card-title fw-bold">{{ info.title }}</h4>
@@ -27,6 +27,10 @@ export default {
             </div>
             <div class="mb-3" v-else>
                 <strong>Nessuna tipologia</strong>
+            </div>
+            <div class="mb-3" v-if="info.technologies">
+                Tecnologie:
+                <span class="badge text-bg-primary ms-1"></span>
             </div>
             <p class="card-text" v-if="info.description">{{ truncateText(info.description) }}</p>
             <div>
