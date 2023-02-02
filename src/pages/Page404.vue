234a96d368c57a3886ff299380d16b264c2604fc
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-02-02 18:11:54 +0100

    Aggiunge componente e rotta pagina 404

---
+++ src/pages/Page404.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: "Page404",
+};
+</script>
+
+<template>
+    <h1 class="text-center">La pagina richiesta non è stata trovata</h1>
+</template>
+
+<style lang="scss" scoped></style>
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import { createRouter, createWebHistory } from "vue-router";
 // importo le pagine (pages)
 import HomePage from './pages/HomePage.vue';
 import AboutUs from './pages/AboutUs.vue';
+import Page404 from './pages/Page404.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -17,7 +18,15 @@ const router = createRouter({
             path: '/chi-siamo',
             name: 'about-us',
             component: AboutUs,
-        }
+        },
+
+        // Rotta che gestisce l'errore della homepage (se l'utente inserisce manualmente una parte url (dopo /) errata)
+        // Questa rotta va inserita sempre al fondo per permettere la ricerca e il controllo di tutte le rotte
+        { 
+            path: '/:pathMatch(.*)*',
+            name: 'Page404',
+            component: Page404,
+        },
     ]
 });
 
